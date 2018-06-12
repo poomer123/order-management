6a0540a749baddd2b4ff82da46831de40d52a606
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-06-12 15:50:25 +0700

    install Antd and create layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
+import { Layout, Menu, Icon } from 'antd'
+const { Header, Sider, Content, Footer } = Layout
+
 
 class App extends Component {
+
+    state = {
+        collapsed: false,
+    }
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h1>Order Management System</h1>
-            </div>
+            <Layout id="components-layout-demo-custom-trigger">
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}
+                >
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                        <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span>nav 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="video-camera" />
+                            <span>nav 2</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="upload" />
+                            <span>nav 3</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Icon
+                            className="trigger"
+                            type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                            onClick={this.toggle}
+                        />
+                    </Header>
+                    <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 800 }}>
+                        Content
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        Order Management Application ©2018 Created by kickdown.in.th
+                    </Footer>
+                </Layout>
+            </Layout>
         )
     }
 }
